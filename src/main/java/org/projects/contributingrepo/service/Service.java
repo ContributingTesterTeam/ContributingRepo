e54b13d3ed0fb71aa94f5507ebf555992cc61787
Bogdan <b.synenko@vector-inc.dev>
2024-08-21 19:41:35 +0300

    mini commit

---
+++ src/main/java/org/projects/contributingrepo/service/Service.java
@@ -0,0 +1,10 @@
+package org.projects.contributingrepo.service;
+
+
+import java.util.Random;
+
+public class Service {
+    public static int randomize() {
+        return new Random().nextInt();
+    }
+}
