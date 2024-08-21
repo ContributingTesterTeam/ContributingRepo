a9064cfca9b0dbf2a3ec8a485e1416582fd685ce
Bogdan <b.synenko@vector-inc.dev>
2024-08-21 19:34:10 +0300

    initial commit

---
+++ src/main/java/org/projects/contributingrepo/ContributingRepoApplication.java
@@ -0,0 +1,13 @@
+package org.projects.contributingrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributingRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContributingRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/projects/contributingrepo/ContributingRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projects.contributingrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributingRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
